25d424693e5d803d590d43b8ae5841c89262c616
I194 <i1948374@yandex.ru>
2021-11-11 14:55:32 +0300

    refactor

--- src/components/ConvertSelector/ConvertSelector.tsx
+++ src/components/ConvertSelector/ConvertSelector.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from "react";
+import React, { FunctionComponent } from "react";
 import styles from "./ConvertSelector.module.scss";
 
 import { toDIR, toPMCSV, toPMM, toPMXLSX } from "../../services/converters/dir";
