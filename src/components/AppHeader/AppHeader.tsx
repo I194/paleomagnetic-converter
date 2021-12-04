88059b891d8139022617ad5fc8e6c8b8e926e79c
I194 <i1948374@yandex.ru>
2021-12-04 12:08:57 +0300

    add navbar

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import styles from "./AppHeader.module.css";
+import styles from "./AppHeader.module.scss";
 
 const AppHeader = () => {
   return (
-    <header className={styles.navPanel}>
-      
-    </header>
+    <nav className={styles.navPanel}>
+      <button className={styles.navBtn}>Результаты чисток</button>
+      <button className={styles.navBtn}>Статистика направлений</button>
+    </nav>
   )
 }
 
